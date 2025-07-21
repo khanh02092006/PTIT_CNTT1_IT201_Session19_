e860d1d515c869af0d8514c6a763ed2765cf1a3f
khanh02092006 <dinhkhanh1881@gmail.com>
2025-07-21 22:55:25 +0700

    ls2

---
+++ PTIT_CNTT1_IT201_Session19_bai2/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef  struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+}node;
+node* create_node(int data)
+{
+    node *newnode=(node*)malloc(sizeof(node));
+    if(newnode==NULL)
+    {
+        printf("chua cap phat");
+        return NULL;
+    }
+    newnode->data=data;
+    newnode->left=NULL;
+    newnode->right=NULL;
+    return newnode;
+}
+//NLR
+void preorder(node *root)
+{
+    if(root==NULL) return;
+    printf("%d ",root->data);
+    preorder(root->left);
+    preorder(root->right);
+
+}
+
+
+int main(void)
+{
+    node *root=create_node(2);
+    root->left=create_node(3);
+    root->right=create_node(4);
+    root->left->left=create_node(5);
+    printf(" duyet theo NLR :");
+    preorder(root);
+    printf("\n");
+    return 0;
+}
